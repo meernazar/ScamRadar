b0b1c509160a9af7d578711dffc850be6b02982b
meernazar <mirnazar53@gmail.com>
2026-05-25 04:54:46 +0500

    Bind app to all network interfaces

--- server.js
+++ server.js
@@ -199,6 +199,7 @@ app.get("/api/health", (_req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
+
   console.log(`ScamRadar running on http://localhost:${PORT}`);
 });
